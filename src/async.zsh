2c2b77813c91b65d81a4602421fd98dcedac502e
JeeBak Kim <jeebak.kim@gmail.com>
2021-08-28 21:13:32 -0700

    Ensure we're using the builtin exec

--- src/async.zsh
+++ src/async.zsh
@@ -11,7 +11,7 @@ _zsh_autosuggest_async_request() {
 	# If we've got a pending request, cancel it
 	if [[ -n "$_ZSH_AUTOSUGGEST_ASYNC_FD" ]] && { true <&$_ZSH_AUTOSUGGEST_ASYNC_FD } 2>/dev/null; then
 		# Close the file descriptor and remove the handler
-		exec {_ZSH_AUTOSUGGEST_ASYNC_FD}<&-
+		builtin exec {_ZSH_AUTOSUGGEST_ASYNC_FD}<&-
 		zle -F $_ZSH_AUTOSUGGEST_ASYNC_FD
 
 		# We won't know the pid unless the user has zsh/system module installed
@@ -21,7 +21,7 @@ _zsh_autosuggest_async_request() {
 	fi
 
 	# Fork a process to fetch a suggestion and open a pipe to read from it
-	exec {_ZSH_AUTOSUGGEST_ASYNC_FD}< <(
+	builtin exec {_ZSH_AUTOSUGGEST_ASYNC_FD}< <(
 		# Tell parent process our pid if we can
 		echo ${sysparams[pid]:-}
 
@@ -67,7 +67,7 @@ _zsh_autosuggest_async_response() {
 		zle autosuggest-suggest -- "$suggestion"
 
 		# Close the fd
-		exec {1}<&-
+		builtin exec {1}<&-
 		_ZSH_AUTOSUGGEST_ASYNC_FD=
 		_ZSH_AUTOSUGGEST_ASYNC_PID=
 	fi
